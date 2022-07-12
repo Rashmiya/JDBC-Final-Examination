fa0419d0d013755d7d9e333a83f66cdbb8ef7546
[200~Rashmiya~ <rashmiyarafeek1200@gmail.com>
2022-07-12 11:18:25 +0530

    Created CrudUtil class for connect with Database

--- src/controller/StudentFormController.java
+++ src/controller/StudentFormController.java
@@ -1,6 +1,8 @@
 package controller;
 
 import com.jfoenix.controls.JFXTextField;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
@@ -10,6 +12,10 @@ import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import model.Student;
+import util.CrudUtil;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class StudentFormController {
     public Button btnDelete;
@@ -30,8 +36,24 @@ public class StudentFormController {
     public JFXTextField txtSearch;
     public Button btnUpdate;
     public TextField txtAddress;
-    public AnchorPane txtNIC;
     public TextField txtNic;
+
+    public void initialize(){
+        try {
+            loadAllStudents();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void loadAllStudents() throws SQLException, ClassNotFoundException {
+        ObservableList<Student> obList = FXCollections.observableArrayList();
+        obList.clear();
+        ResultSet result = CrudUtil.execute("SELECT * FROM student ORDER BY student_ID ASC");
+
+    }
 
     public void UpdateOnAction(ActionEvent actionEvent) {
     }
